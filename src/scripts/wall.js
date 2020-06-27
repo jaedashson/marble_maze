1999d14f5c7cb79719d9a69798dd59a1f703b8da
Jae-Son Song <jaesonsong@gmail.com>
2020-06-27 01:33:07 -0400

    fix wall intersections

--- src/scripts/wall.js
+++ src/scripts/wall.js
@@ -1,15 +1,21 @@
 import { CELL_SIZE, WALL_RADIUS } from "../index"; // Why don't these imports work?
 
+// modStart > 0 => extend the starting portion of the wall
+// modStart < 0 => shorten the starting portion of the wall
+// modEnd > 0 => extend the ending portion of the wall
+// modEnd < 0 => shorten the ending portion of the wall
+
 export default class Wall {
-  constructor(startXCell, startYCell, orientation, length, cellSize, wallRadius) {
+  constructor(startXCell, startYCell, orientation, length, cellSize, wallRadius, modStart, modEnd) {
     debugger // FIXME
     this.startXCell = startXCell;
     this.startYCell = startYCell;
     this.orientation = orientation;
     this.length = length;
     this.cellSize = cellSize;
-    this.wallRadius = wallRadius;
     this.radius = wallRadius;
+    this.modStart = modStart;
+    this.modEnd = modEnd;
 
     // Calculate dimensions
     this.topLeft;
@@ -29,6 +35,15 @@ export default class Wall {
         x: this.startXCell * this.cellSize,
         y: this.startYCell * this.cellSize + this.radius
       };
+      
+      if (this.modStart < 0) {
+        this.topLeft.x += this.radius;
+        this.bottomLeft.x += this.radius;
+      } else if (this.modStart > 0) {
+        this.topLeft.x -= this.radius;
+        this.bottomLeft.x -= this.radius;
+      }
+
       this.topRight = {
         x: (this.startXCell + this.length) * this.cellSize,
         y: this.startYCell * this.cellSize - this.radius
@@ -38,8 +53,15 @@ export default class Wall {
         y: this.startYCell * this.cellSize + this.radius
       };
 
+      if (this.modEnd < 0) {
+        this.topRight.x -= this.radius;
+        this.bottomRight.x -= this.radius;
+      } else if (this.modEnd > 0) {
+        this.topRight.x += this.radius;
+        this.bottomRight.x += this.radius;
+      }
+
     } else if (this.orientation === "y") {
-      debugger // FIXME
       this.topLeft = {
         x: this.startXCell * this.cellSize - this.radius,
         y: this.startYCell * this.cellSize
@@ -48,6 +70,15 @@ export default class Wall {
         x: this.startXCell * this.cellSize + this.radius,
         y: this.startYCell * this.cellSize
       };
+
+      if (this.modStart < 0) {
+        this.topLeft.y += this.radius;
+        this.topRight.y += this.radius;
+      } else if (this.modStart > 0) {
+        this.topLeft.y -= this.radius;
+        this.topRight.y -= this.radius;
+      }
+
       this.bottomLeft = {
         x: this.startXCell * this.cellSize - this.radius,
         y: (this.startYCell + this.length) * this.cellSize 
@@ -56,11 +87,19 @@ export default class Wall {
         x: this.startXCell * this.cellSize + this.radius,
         y: (this.startYCell + this.length) * this.cellSize
       };
+
+      if (this.modEnd < 0) {
+        this.bottomLeft.y -= this.radius;
+        this.bottomRight.y -= this.radius;
+      } else if (this.modEnd > 0) {
+        this.bottomLeft.y += this.radius;
+        this.bottomRight.y += this.radius;
+      }
+
     }
   }
 
   draw(ctx) {
-    debugger // FIXME
     ctx.fillStyle = "black";
     ctx.fillRect(this.topLeft.x, this.topLeft.y, this.topRight.x - this.topLeft.x, this.bottomLeft.y - this.topLeft.y);
   }
