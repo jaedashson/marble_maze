632d2440d70f1dc810ae15d8dbabb725a207b8b8
Jae-Son Song <jaesonsong@gmail.com>
2020-07-11 23:50:24 -0400

    save progress

--- src/scripts/hole.js
+++ src/scripts/hole.js
@@ -4,10 +4,17 @@ export default class Hole {
     this.id = id;
     this.posCellX = posCellX;
     this.posCellY = posCellY;
+    this.posX = this.posCellX * this.cellSize;
+    this.posY = this.posCellY * this.cellSize;
+
+
     this.radius = (this.cellSize * 0.75) / 2
   }
 
-  draw() {
-    
+  draw(ctx) {
+    ctx.fillStyle = "gray";
+    ctx.beginPath();
+    ctx.arc(this.posX, this.posY, this.radius, 0, 2 * Math.PI);
+    ctx.fill();
   }
 }
